d869802ad00d7c0fc5c21c0be901afe1a1c219c3
Slonetech <kengeregordon@gmail.com>
2023-08-22 06:42:15 +0300

    p3-attributes and properties completed

--- lib/dog.py
+++ lib/dog.py
@@ -1,15 +1,38 @@
-#!/usr/bin/env python3
-
-APPROVED_BREEDS = [
-    "Mastiff",
-    "Chihuahua",
-    "Corgi",
-    "Shar Pei",
-    "Beagle",
-    "French Bulldog",
-    "Pug",
-    "Pointer"
-]
-
 class Dog:
-    pass
+    APPROVED_BREEDS = [
+        "Mastiff",
+        "Chihuahua",
+        "Corgi",
+        "Shar Pei",
+        "Beagle",
+        "French Bulldog",
+        "Pug",
+        "Pointer"
+    ]
+
+    def __init__(self, name, breed):
+        self._name = name
+        self._breed = breed
+
+    def name(self):
+        return self._name
+
+    def name(self, value):
+        if isinstance(value, str) and 1 <= len(value) <= 25:
+            self._name = value
+        else:
+            print("Name must be a string between 1 and 25 characters.")
+
+    def breed(self):
+        return self._breed
+
+    def breed(self, value):
+        if value in Dog.APPROVED_BREEDS:
+            self._breed = value
+        else:
+            print("Breed must be in list of approved breeds.")
+
+
+fido = Dog("Fido", "Mastiff")
+
+fido.name = ""
