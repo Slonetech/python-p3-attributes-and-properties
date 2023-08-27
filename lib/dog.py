4caf12d3c076c9a9efbf5791a4d4c3603c3f5a32
Slonetech <kengeregordon@gmail.com>
2023-08-27 15:20:19 +0300

    completed assignment

--- lib/dog.py
+++ lib/dog.py
@@ -10,27 +10,29 @@ class Dog:
         "Pointer"
     ]
 
-    def __init__(self, name = ''):
+    def __init__(self, name='', breed=''):
         self._name = name
+        self._breed = breed
 
-    def get_name(self):
+        
+    @property
+    def name(self):
         return self._name
-
-    def set_name(self, name):
-        if (type(name) == str) and (1 <= len(name) <= 25):
-            print(f"Setting name to {name}")
-            self._name = name
+    
+    @name.setter
+    def name(self, value):
+        if not (isinstance(value, str) and 1 <= len(value) <= 25):
+            print("Name must be a string between 1 and 25 characters.")
         else:
-            print("Name must be string between 1 and 25 characters.")
+            self._name = value
 
-    def get_breed(self):
+    @property
+    def breed(self):
         return self._breed
-
-    def set_breed(self, breed):
-        if breed in Dog.approved_breeds:
-            self._breed = breed
-        else:
+    
+    @breed.setter
+    def breed(self, value):
+        if value not in Dog.APPROVED_BREEDS:
             print("Breed must be in list of approved breeds.")
-
-    name = property(get_name, set_name)
-    breed = property(get_breed, set_breed)
+        else:
+            self._breed = value
